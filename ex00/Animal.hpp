a6cea7a12baa551c89b82b13633530ac33fdf21f
Hassan Lakhal <hlakhal-@e1r4p12.1337.ma>
2023-10-27 00:11:55 +0100

    fixed ex00

---
+++ ex00/Animal.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 22:08:56 by hlakhal-          #+#    #+#             */
+/*   Updated: 2023/10/26 23:03:38 by hlakhal-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include<iostream>
+
+class Animal
+{
+protected:
+    std::string type;
+public:
+    Animal();
+    Animal(const std::string& type);
+    ~Animal();
+    const std::string& getType() const;
+    virtual void makeSound() const;
+};
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 22:09:10 by hlakhal-          #+#    #+#             */
+/*   Updated: 2023/10/26 22:37:07 by hlakhal-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"Animal.hpp"
+#include"Dog.hpp"
+#include"Cat.hpp"
+
+int main()
+{
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    j->makeSound();
+    meta->makeSound();
+return 0;
+}
