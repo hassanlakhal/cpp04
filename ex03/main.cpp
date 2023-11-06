bb161a705507a8915d2a830fde90c733a9882819
Hassan Lakhal <hlakhal-@e1r5p10.1337.ma>
2023-11-06 20:33:05 +0100

    fixed errors

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 16:54:32 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/11/06 07:25:07 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/11/06 20:29:16 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,34 +17,28 @@
 
 
 
-int main1()
+int main()
 {
     IMateriaSource* src = new MateriaSource();
     src->learnMateria(new Ice());
     src->learnMateria(new Cure());
+    
     ICharacter* me = new Character("me");
     AMateria* tmp;
+    
     tmp = src->createMateria("ice");
     me->equip(tmp);
     tmp = src->createMateria("cure");
     me->equip(tmp);
-    AMateria* tmp1;
-    tmp1 = src->createMateria("test");
-    me->equip(tmp1);
     ICharacter* bob = new Character("bob");
+    
     me->use(0, *bob);
     me->use(1, *bob);
     me->use(2, *bob);
-
+    
     delete bob;
     delete me;
     delete src;
     
     return 0;
-}
-
-int main()
-{
-    main1();
-    // system("leaks A_animal");
 }
